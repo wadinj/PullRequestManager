4011d8b76e0285cefd697efc3d33cac0c195505a
WADIN Jonathan <wadin.jonathan@gmail.com>
2016-09-21 23:24:48 +0200

    [evo] Ajout du premier controller dashboard
    > Mise en place des resources sous la page index.jsp avec la JSTL c:url
    > Refonte de l'arborescence du site
    > Création du controller Dashboard qui redirige pour l'instant vers
    index.jsp
    > Ajout des logs, il faut remplacer votre standalone.xml du serveur
    JBOSS

---
+++ PRM-Web/src/main/java/com/escowad/prm/controllers/DashboardController.java
@@ -0,0 +1,25 @@
+package com.escowad.prm.controllers;
+
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class DashboardController {
+
+	Logger logger = Logger.getLogger(DashboardController.class);
+	@Autowired
+	private HttpServletRequest context;
+	
+	@RequestMapping(value = "/dashboard", method = RequestMethod.GET)
+	public String dashboard(ModelMap model) {
+		logger.info("Redirection vers l'index dashboard");
+		return "index";
+	}
+}
