8fe374bb21340c1253044bd64468c01cbfe5d192
Jonathan WADIN <wadin.jonathan@gmail.com>
2016-10-08 17:21:23 +0200

    (EVO) Ajouter var session plugin List

--- PRM-Service/src/main/java/com/escowad/prm/utils/ConstantUtils.java
+++ PRM-Service/src/main/java/com/escowad/prm/utils/ConstantUtils.java
@@ -7,7 +7,7 @@ public class ConstantUtils {
 	public static final String ID_CODE_FROM_GITHUB = "CODE_ID_GITHUB";
 	public static final String ID_SESSION_USERGIT = "USERGIT";
 	public static final String ID_SESSION_REPOS = "GITHUB_REPOS";
-	
+	public static final String ID_SESSION_PLUGINS = "PLUGINS_LIST";
 	public static final String ID_SESSION_PULLREQUESTS= "PULLREQUEST_PROJECTS";
 	public static final String ID_URL_CLI_GITHUB = "client_id";
 	public static final String ID_URL_SECRET_GITHUB = "secret_id";
